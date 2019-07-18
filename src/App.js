f3351f85cc80580ec82634efa482b1997dcf9df3
Gabriel Machado <g_machado1@hotmail.com>
2019-07-18 08:47:41 -0300

    Implementado AutorBox para reunir componentes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,11 @@
 import React, { Component } from 'react';
-import $ from "jquery";
-import InputCustomizado from "./componentes/InputCustomizado";
-import BotaoSubmitCustomizado from "./componentes/BotaoSubmitCustomizado";
+import AutorBox from './Autor';
 import './css/pure-min.css';
 import './css/side-menu.css';
 
 class App extends Component {
-  
-  constructor() {
-    super();
-
-    this.state = {
-      lista: [],
-      nome: '',
-      email: '',
-      senha: ''
-    };
-
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: 'https://cdc-react.herokuapp.com/api/autores',
-      dataType: 'json',
-      success: function(resposta) {
-        this.setState({ lista: resposta });
-      }.bind(this)
-    });
-  }
-
-  enviaForm(evento) {
-    evento.preventDefault();
-
-    $.ajax({
-      url:'https://cdc-react.herokuapp.com/api/autores',
-      contentType:'application/json',
-      dataType:'json',
-      type:'post',
-      data: JSON.stringify({
-        nome: this.state.nome,
-        email: this.state.email,
-        senha: this.state.senha
-      }),
-      success: function(resposta){
-        this.setState({ lista: resposta })
-        }.bind(this),
-      error: function(resposta){
-        console.log("erro");
-      }      
-    });
-  }
-
-  setNome(evento) {
-    this.setState({ nome: evento.target.value });
-  }
-
-  setEmail(evento) {
-    this.setState({ email: evento.target.value });
-  }
-
-  setSenha(evento) {
-    this.setState({ senha: evento.target.value });
-  }
 
   render() { 
-
     return (
       <div id="layout">
       
@@ -96,37 +33,7 @@ class App extends Component {
             <h1>Cadastro de Autores</h1>
           </div>
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
-                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
-                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
-                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
-                <BotaoSubmitCustomizado label="Gravar"/>
-              </form>             
-
-            </div>  
-            <div>            
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.map(autor => {
-                      return (
-                        <tr key={autor.id}>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table> 
-            </div>             
+            <AutorBox />
           </div>
         </div>            
       </div>
