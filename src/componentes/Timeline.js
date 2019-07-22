dfeb0c42de821cb0c2c4c8c4e87c1e6f4cc2cedf
Gabriel <gabriel.machado@accurate.com.br>
2019-07-22 16:05:59 -0300

    Adicionado fetch de dados da api e deixando listagem dinâmica

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
-    render(){
-        return (
-        <div className="fotos container">
-          <FotoItem/>
-          <FotoItem/>
-        </div>            
-        );
-    }
+
+  constructor() {
+    super();
+    this.state = {fotos: []};
+  }
+  
+  componentDidMount() {
+    fetch('https://instalura-api.herokuapp.com/api/public/fotos/alots')
+      .then(response => response.json())
+      .then(fotos => {
+        this.setState({fotos: fotos});
+      });
+  }
+
+  render(){
+      return (
+      <div className="fotos container">
+        {
+          this.state.fotos.map(foto => <FotoItem foto={foto}/>)
+        }
+      </div>            
+      );
+  }
 }
