5382b5ad2fc682c9cbbcf983b945c6911f72b43b
Gabriel <gabriel.machado@accurate.com.br>
2019-07-22 17:24:51 -0300

    Começado a implementação de autenticação

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './componentes/Login';
 import * as serviceWorker from './serviceWorker';
 import './css/timeline.css';
+import './css/login.css';
 import './css/reset.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter history={history}>
+    <Route path="/" exact component={Login} />
+    <Route path="/timeline" component={App} />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
