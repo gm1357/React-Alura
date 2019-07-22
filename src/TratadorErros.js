90d7baff0e544d981f83f04cc4ba7971b3032e08
Gabriel <gabriel.machado@accurate.com.br>
2019-07-22 15:30:06 -0300

    Reinicializado projeto para segunda parte do curso de react

--- src/TratadorErros.js
+++
@@ -1,8 +0,0 @@
-import PubSub from 'pubsub-js';
-
-export default class TratadorErros {
-
-  publicaErros(erros) {
-    erros.errors.map(erro => PubSub.publish('erro-validacao', erro));
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import Home from './Home';
-import AutorBox from './Autor';
-import LivroBox from './Livro';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './css/timeline.css';
+import './css/reset.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/autor" component={AutorBox} />
-        <Route path="/livro" component={LivroBox}/>
-      </Switch>
-    </App>
-  </BrowserRouter>, 
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
